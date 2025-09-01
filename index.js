b8c480647ac98161e290fc3ce78fd2978cfa98e6
PuneetVerma07 <pk2382849@gmail.com>
2025-09-01 11:44:35 +0530

    Add Discord bot client setup with intents and ready event

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 require("dotenv").config();
-const {} = require("discord.js")
+
+const { Client, GatewayIntentBits} = require("discord.js")
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+client.once("clientReady", () => {
+    console.log("Bot is ready!")
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
