38d36bb1873d84d03742ef0c5869db41d0d5db3e
PuneetVerma07 <pk2382849@gmail.com>
2025-12-03 16:50:14 +0530

    create a server with the help of express

--- app.js
+++
@@ -1,9 +0,0 @@
-const http = require("http")
-
-const server = http.createServer((req, res) => {
-    res.end("Hello world from the server side")
-});
-
-server.listen(3000, () => {
-    console.log("Server is running on port no.. 3000")
-})
