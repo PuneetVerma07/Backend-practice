af95f87607087c9801e5f241ee4ba9e14f312747
PuneetVerma07 <pk2382849@gmail.com>
2025-12-04 11:31:31 +0530

    note's application's CRUD operations completed

--- server.js
+++ server.js
@@ -1,21 +1,45 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-let notes = []
+let notes = [];
 
 app.post("/notes", (req, res) => {
-    console.log(req.body)
-    notes.push(req.body)
+  console.log(req.body);
+  notes.push(req.body);
 
-    res.json({
-        message: "notes added successfully",
-        notes: notes
-    })
-})
+  res.json({
+    message: "note created successfullyf",
+  });
+});
+
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+
+  delete notes[index];
+
+  res.json({
+    message: "note deleted",
+  });
+});
+
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const { title } = req.body;
+
+  notes[index].title = title;
+
+  res.json({
+    message: "note updated successfully.",
+  });
+});
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+  console.log(`Server is running on http://localhost:3000`);
+});
